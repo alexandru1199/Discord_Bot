577fe205aa5db268490810b95de2a7481cf28e22
alexandru1199 <sirbualexvladut3@yahoo.com>
2021-07-16 13:01:19 +0300

    Delete COMENZIGENERALE directory

--- COMENZIGENERALE/play.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-    name:"play",
-    description:"plays a song ",
-     async execute(message, args, bot, currency, Users, Op, CurrencyShop, commandArgs, xp, lv, Reflect, serverQueue,ytdl,queue){
-
-        
-    } }
--- COMENZIGENERALE/userid.js
+++
@@ -1,9 +0,0 @@
-module.exports= {
-    name:"userid",
-    alliases:['id'],
-    description:"cand a fost userul creat",
-    execute(message){
-       message.channel.send("id-ul tau este :" +message.author.id)
-    }
-
-}
